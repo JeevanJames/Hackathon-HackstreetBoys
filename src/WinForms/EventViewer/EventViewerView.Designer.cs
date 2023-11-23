6e67f0ee4897c5c1c8b680792466f4d60229bd94
Jeevan James <jeevanjj@live.com>
2023-11-23 11:35:42 +0530

    Add basic event viewer functionality

--- src/WinForms/EventViewer/EventViewerView.Designer.cs
+++ src/WinForms/EventViewer/EventViewerView.Designer.cs
@@ -28,16 +28,73 @@ partial class EventViewerView
     /// </summary>
     private void InitializeComponent()
     {
+        var resources = new System.ComponentModel.ComponentResourceManager(typeof(EventViewerView));
+        txtEvents = new TextBox();
+        toolStripEvents = new ToolStrip();
+        ttxtSources = new ToolStripTextBox();
+        tbtnRefresh = new ToolStripButton();
+        toolStripEvents.SuspendLayout();
         SuspendLayout();
+        // 
+        // txtEvents
+        // 
+        txtEvents.BackColor = SystemColors.Info;
+        txtEvents.Dock = DockStyle.Fill;
+        txtEvents.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+        txtEvents.ForeColor = SystemColors.InfoText;
+        txtEvents.Location = new Point(0, 25);
+        txtEvents.Multiline = true;
+        txtEvents.Name = "txtEvents";
+        txtEvents.ReadOnly = true;
+        txtEvents.ScrollBars = ScrollBars.Both;
+        txtEvents.Size = new Size(600, 375);
+        txtEvents.TabIndex = 0;
+        txtEvents.WordWrap = false;
+        // 
+        // toolStripEvents
+        // 
+        toolStripEvents.GripStyle = ToolStripGripStyle.Hidden;
+        toolStripEvents.Items.AddRange(new ToolStripItem[] { ttxtSources, tbtnRefresh });
+        toolStripEvents.Location = new Point(0, 0);
+        toolStripEvents.Name = "toolStripEvents";
+        toolStripEvents.Size = new Size(600, 25);
+        toolStripEvents.TabIndex = 1;
+        toolStripEvents.Text = "toolStrip1";
+        // 
+        // ttxtSources
+        // 
+        ttxtSources.Name = "ttxtSources";
+        ttxtSources.Size = new Size(200, 25);
+        // 
+        // tbtnRefresh
+        // 
+        tbtnRefresh.DisplayStyle = ToolStripItemDisplayStyle.Text;
+        tbtnRefresh.Image = (Image)resources.GetObject("tbtnRefresh.Image");
+        tbtnRefresh.ImageTransparentColor = Color.Magenta;
+        tbtnRefresh.Name = "tbtnRefresh";
+        tbtnRefresh.Size = new Size(50, 22);
+        tbtnRefresh.Text = "&Refresh";
+        tbtnRefresh.Click += tbtnRefresh_Click;
         // 
         // EventViewerView
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
+        Controls.Add(txtEvents);
+        Controls.Add(toolStripEvents);
         Name = "EventViewerView";
         Size = new Size(600, 400);
+        Load += EventViewerView_Load;
+        toolStripEvents.ResumeLayout(false);
+        toolStripEvents.PerformLayout();
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private TextBox txtEvents;
+    private ToolStrip toolStripEvents;
+    private ToolStripTextBox ttxtSources;
+    private ToolStripButton tbtnRefresh;
 }
--- src/WinForms/EventViewer/EventViewerView.cs
+++ src/WinForms/EventViewer/EventViewerView.cs
@@ -1,18 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Hackathon.HackstreetBoys.WinForms.EventViewer;
+
 public partial class EventViewerView : UserControl
 {
     public EventViewerView()
     {
         InitializeComponent();
+    }
+
+    private void EventViewerView_Load(object sender, EventArgs e)
+    {
+        RefreshLogs();
+    }
+
+    private void tbtnRefresh_Click(object sender, EventArgs e)
+    {
+        RefreshLogs(ttxtSources.Text);
+    }
+
+    private void RefreshLogs(string? sourcesStr = null)
+    {
+        StringBuilder sb = new();
+
+        using EventLog log = new("Application");
+        sb.AppendLine(log.Source);
+
+        string[] sources = string.IsNullOrWhiteSpace(sourcesStr)
+            ? Array.Empty<string>()
+            : sourcesStr.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        if (sources.Length == 0)
+            sources = Array.Empty<string>();
+
+        IEnumerable<EventLogEntry> logs = log.Entries.Cast<EventLogEntry>();
+        if (sources.Length > 0)
+            logs = logs.Where(e => sources.Any(s => e.Source.Contains(s, StringComparison.OrdinalIgnoreCase)));
+        logs = logs.Take(100);
+        foreach (EventLogEntry entry in logs)
+            sb.AppendLine($"[{entry.TimeWritten:s} {entry.EntryType}] {entry.InstanceId} | {entry.Source} | {entry.Message}");
+
+        txtEvents.Text = sb.ToString();
     }
 }
